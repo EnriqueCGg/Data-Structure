eb91207c23e7c0814b9489eee7ac6fbd057bc836
emmacgo <emmanuelcgo01@gmail.com>
2025-09-29 21:31:04 -0600

    Agregué Inorder, Postorder y búsqueda

--- arbol.py
+++ arbol.py
@@ -33,16 +33,64 @@ class ArbolB():
             
     def preorder_rec(self, nodo):
         if nodo:
-            print(nodo.dato)
+            print(nodo.dato, end=', ')
             self.preorder_rec(nodo.izq)
             self.preorder_rec(nodo.der)
 
-            
-                
+    def inorder(self):
+        self.inorder_rec(self.raiz)
+
+    def inorder_rec(self, nodo):
+        if nodo:
+            self.inorder_rec(nodo.izq)
+            print(nodo.dato, end=', ')
+            self.inorder_rec(nodo.der)
+
+    def postorder(self):
+        self.postorder_rec(self.raiz)
+
+    def postorder_rec(self, nodo):
+        if nodo:
+            self.postorder_rec(nodo.izq)
+            self.postorder_rec(nodo.der)
+            print(nodo.dato, end=', ')
+
+    def buscar(self, dato):
+        return self.buscar_rec(dato, self.raiz)
+
+    def buscar_rec(self, dato, nodo):
+        if nodo is None:
+            return False
+        if nodo.dato == dato:
+            return True
+        elif dato < nodo.dato:
+            return self.buscar_rec(dato, nodo.izq)
+        else:
+            return self.buscar_rec(dato, nodo.der)
+#test
 arbol = ArbolB()
+arbol.insertar(20)
 arbol.insertar(15)
-arbol.insertar(7)
+arbol.insertar(33)
+arbol.insertar(10)
+arbol.insertar(18)
+arbol.insertar(5)
+arbol.insertar(12)
+arbol.insertar(17)
 arbol.insertar(19)
-arbol.insertar(3)
-arbol.insertar(11)
+arbol.insertar(25)
+arbol.insertar(50)
+arbol.insertar(21)
+arbol.insertar(27)
+arbol.insertar(40)
+arbol.insertar(70)
+
+print('Preorder: ')
 arbol.preorder()
+print('\nInorder: ')
+arbol.inorder()
+print('\nPostorder: ')
+arbol.postorder()
+
+print("\nBuscar 25:", arbol.buscar(25))  
+print("Buscar 99:", arbol.buscar(99))    
